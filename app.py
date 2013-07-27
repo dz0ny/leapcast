8f2c3c3b0c12796c63a233cf15ba137cacb71568
dz0ny <janez.troha@gmail.com>
2013-07-27 12:22:42 +0200

    add play mode

--- app.py
+++ app.py
@@ -13,13 +13,17 @@ import argparse
 import signal
 import logging
 from textwrap import dedent
-import sys
+import shlex, subprocess
 
 yt_status = dict(state="stopped", link="")
 cc_status = dict(state="stopped", link="")
 pm_status = dict(state="stopped", link="")
 gm_status = dict(state="stopped", link="")
+gcsa_status = dict(state="stopped", link="")
+gcp_status = dict(state="stopped", link="")
+
 friendlyName = "Mopidy"
+user_agent ="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) CrKey/30.0.1573.2 Safari/537.36"
 
 class SSDP(DatagramProtocol):
     SSDP_ADDR = '239.255.255.250'
@@ -54,6 +58,7 @@ class LEAP(tornado.web.RequestHandler):
 
     service = None
     ip = None
+    url = "$query"
 
     def prepare(self):
         self.ip = self.request.host
@@ -67,9 +72,20 @@ class LEAP(tornado.web.RequestHandler):
     def post(self):
         self.set_status(201, "Created")
         self.set_header("Location", self._getLocation(self.__class__.__name__))
+        self.launch(self.request.body)
 
     def _getLocation(self, app):
         return "http://%s/apps/%s/run" % (self.ip, app )
+
+    def launch(self, data):
+        appurl = string.Template(self.url).substitute(query=data)
+        command_line ="""/opt/google/chrome/chrome --app="%s" --user-agent="%s"  """  % (appurl, user_agent)
+        print(command_line)
+        args = shlex.split(command_line)
+        self.pid = subprocess.Popen(args)
+
+    def destroy(self, pid):
+        pid.terminate()
 
     def _toXML(self, data):
         return string.Template(dedent(self.service)).substitute(data)
@@ -96,6 +112,7 @@ class ChromeCast(LEAP):
         $link
     </service>
     """
+    url = "https://www.gstatic.com/cv/receiver.html?$query"
 
     def get(self):
         global cc_status
@@ -120,6 +137,7 @@ class YouTube(LEAP):
         $link
     </service>
     """
+    url = "https://www.youtube.com/tv?$query"
 
     def get(self):
         global yt_status
@@ -128,7 +146,7 @@ class YouTube(LEAP):
     def post(self):
         global yt_status
         super(YouTube, self).post()
-        yt_status = dict(state="running", link="""<link rel="run" href="run"/>""")
+        yt_status = dict(state="running", link="""<link rel="run" href="run"/>""", pid=self.pid)
         self._response(yt_status)
 
     def delete(self):
@@ -144,6 +162,7 @@ class PlayMovies(LEAP):
         $link
     </service>
     """
+    url = "https://play.google.com/video/avi/eureka?$query"
 
     def get(self):
         global pm_status
@@ -168,7 +187,7 @@ class GoogleMusic(LEAP):
         $link
     </service>
     """
-
+    url = "https://play.google.com/music/cast/player"
     def get(self):
         global gm_status
         self._response(gm_status)
@@ -183,6 +202,54 @@ class GoogleMusic(LEAP):
         global gm_status
         gm_status = dict(state="stopped", link="")
         self._response(gm_status)
+
+class GoogleCastSampleApp(LEAP):
+    service = """<service xmlns="urn:dial-multiscreen-org:schemas:dial">
+        <name>GoogleCastSampleApp</name>
+        <options allowStop="true"/>
+        <state>$state</state>
+        $link
+    </service>
+    """
+    url = "http://anzymrcvr.appspot.com/receiver/anzymrcvr.html"
+    def get(self):
+        global gcsa_status
+        self._response(gcsa_status)
+
+    def post(self):
+        global gcsa_status
+        super(GoogleCastSampleApp, self).post()
+        gcsa_status = dict(state="running", link="""<link rel="run" href="run"/>""")
+        self._response(gcsa_status)
+
+    def delete(self):
+        global gcsa_status
+        gcsa_status = dict(state="stopped", link="")
+        self._response(gcsa_status)
+
+class GoogleCastPlayer(LEAP):
+    service = """<service xmlns="urn:dial-multiscreen-org:schemas:dial">
+        <name>GoogleCastPlayer</name>
+        <options allowStop="true"/>
+        <state>$state</state>
+        $link
+    </service>
+    """
+    url = "http://anzymrcvr.appspot.com/receiver/anzymrcvr.html"
+    def get(self):
+        global gcp_status
+        self._response(gcp_status)
+
+    def post(self):
+        global gcp_status
+        super(GoogleCastPlayer, self).post()
+        gcp_status = dict(state="running", link="""<link rel="run" href="run"/>""")
+        self._response(gcp_status)
+
+    def delete(self):
+        global gcp_status
+        gcp_status = dict(state="stopped", link="")
+        self._response(gcp_status)
 
 class DeviceHandler(tornado.web.RequestHandler):
 
@@ -222,6 +289,8 @@ class DeviceHandler(tornado.web.RequestHandler):
             LEAP.toInfo(YouTube.service, yt_status),
             LEAP.toInfo(PlayMovies.service, pm_status),
             LEAP.toInfo(GoogleMusic.service, gm_status),
+            LEAP.toInfo(GoogleCastSampleApp.service, gcsa_status),
+            LEAP.toInfo(GoogleCastPlayer.service, gcp_status),
         ])
         self.write(string.Template(dedent(self.device)).substitute(dict(services=gservice, friendlyName=friendlyName )))
 
@@ -246,6 +315,8 @@ class HTTPThread(threading.Thread):
             (r"/apps/YouTube", YouTube),
             (r"/apps/PlayMovies", PlayMovies),
             (r"/apps/GoogleMusic", GoogleMusic),
+            (r"/apps/GoogleCastSampleApp", GoogleCastSampleApp),
+            (r"/apps/GoogleCastPlayer", GoogleCastPlayer),
             (r"/connection", WebSocketCast),
             (r"/apps", DeviceHandler),
         ])
