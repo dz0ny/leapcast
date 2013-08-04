855d682b02166f5ea67d141c5bccb883f3d82844
janez troha <janez.troha@gmail.com>
2013-08-04 08:56:03 +0200

    Generate static uuid based on name

--- leapcast/environment.py
+++ leapcast/environment.py
@@ -7,12 +7,12 @@ import uuid
 class Environment(object):
     channels = dict()
     global_status = dict()
-    friendlyName = "leapcast"
-    user_agent = "Mozilla/5.0 (CrKey - 0.9.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1573.2 Safari/537.36"
-    chrome = "/usr/bin/chromium-browser"
+    friendlyName = 'leapcast'
+    user_agent = 'Mozilla/5.0 (CrKey - 0.9.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1573.2 Safari/537.36'
+    chrome = '/usr/bin/chromium-browser'
     fullscreen = False
     interface = None
-    uuid = str(uuid.uuid4())
+    uuid = None
 
 
 def parse_cmd():
@@ -26,15 +26,22 @@ def parse_cmd():
 
     if args.name:
         Environment.friendlyName = args.name
-        logging.info("Service name is %s" % Environment.friendlyName)
+        logging.info('Service name is %s' % Environment.friendlyName)
 
     if args.user_agent:
         Environment.user_agent = args.user_agent
-        logging.info("User agent is %s" % args.user_agent)
+        logging.info('User agent is %s' % args.user_agent)
 
     if args.chrome:
         Environment.chrome = args.chrome
-        logging.info("Chrome path is %s" % args.chrome)
+        logging.info('Chrome path is %s' % args.chrome)
 
     if args.fullscreen:
         Environment.fullscreen = True
+
+    generate_uuid()
+
+
+def generate_uuid():
+    Environment.uuid = str(uuid.uuid5(
+        uuid.NAMESPACE_DNS, ('device.leapcast.%s' % Environment.friendlyName).encode('utf8')))
