d485da416ad61e965c99d6759f74b6dc1c20a5a3
janez troha <janez.troha@gmail.com>
2013-08-04 08:57:11 +0200

    Fix encoding error closes #19

--- leapcast/services/leap.py
+++ leapcast/services/leap.py
@@ -22,8 +22,8 @@ class Browser(object):
             appurl = '--app="%s"' % appurl
         command_line = '''%s --incognito --no-first-run --kiosk --user-agent="%s"  %s''' % (
             Environment.chrome, Environment.user_agent, appurl)
-        args = shlex.split(command_line)
-        self.tmpdir = tempfile.mkdtemp(prefix="leapcast-")
+        args = shlex.split(command_line.encode('utf8'))
+        self.tmpdir = tempfile.mkdtemp(prefix='leapcast-')
         args.append('--user-data-dir=%s' % self.tmpdir)
         self.pid = subprocess.Popen(args)
 
@@ -41,22 +41,22 @@ class Browser(object):
 
 class LEAP(tornado.web.RequestHandler):
     application_status = dict(
-        name="",
-        state="stopped",
-        link="",
+        name='',
+        state='stopped',
+        link='',
         browser=None,
-        connectionSvcURL="",
-        protocols="",
+        connectionSvcURL='',
+        protocols='',
         app=None
     )
-    service = '''<?xml version="1.0" encoding="UTF-8"?>
-    <service xmlns="urn:dial-multiscreen-org:schemas:dial">
+    service = '''<?xml version='1.0' encoding='UTF-8'?>
+    <service xmlns='urn:dial-multiscreen-org:schemas:dial'>
         <name>$name</name>
-        <options allowStop="true"/>
-        <activity-status xmlns="urn:chrome.google.com:cast">
+        <options allowStop='true'/>
+        <activity-status xmlns='urn:chrome.google.com:cast'>
             <description>Legacy</description>
         </activity-status>
-        <servicedata xmlns="urn:chrome.google.com:cast">
+        <servicedata xmlns='urn:chrome.google.com:cast'>
             <connectionSvcURL>$connectionSvcURL</connectionSvcURL>
             <protocols>$protocols</protocols>
         </servicedata>
@@ -66,8 +66,8 @@ class LEAP(tornado.web.RequestHandler):
     '''
 
     ip = None
-    url = "$query"
-    supported_protocols = ["ramp"]
+    url = '$query'
+    supported_protocols = ['ramp']
 
     @property
     def protocols(self):
@@ -78,7 +78,7 @@ class LEAP(tornado.web.RequestHandler):
 
     def get_status_dict(self):
         status = copy.deepcopy(self.application_status)
-        status["name"] = self.get_name()
+        status['name'] = self.get_name()
         return status
 
     def prepare(self):
@@ -89,15 +89,15 @@ class LEAP(tornado.web.RequestHandler):
 
     def set_app_status(self, app_status):
 
-        app_status["name"] = self.get_name()
+        app_status['name'] = self.get_name()
         Environment.global_status[self.get_name()] = app_status
 
     def _response(self):
-        self.set_header("Content-Type", "application/xml")
+        self.set_header('Content-Type', 'application/xml')
         self.set_header(
-            "Access-Control-Allow-Method", "GET, POST, DELETE, OPTIONS")
-        self.set_header("Access-Control-Expose-Headers", "Location")
-        self.set_header("Cache-control", "no-cache, must-revalidate, no-store")
+            'Access-Control-Allow-Method', 'GET, POST, DELETE, OPTIONS')
+        self.set_header('Access-Control-Expose-Headers', 'Location')
+        self.set_header('Cache-control', 'no-cache, must-revalidate, no-store')
         self.finish(self._toXML(self.get_app_status()))
 
     @tornado.web.asynchronous
@@ -105,17 +105,17 @@ class LEAP(tornado.web.RequestHandler):
         '''Start app'''
         self.clear()
         self.set_status(201)
-        self.set_header("Location", self._getLocation(self.get_name()))
+        self.set_header('Location', self._getLocation(self.get_name()))
         status = self.get_app_status()
-        if status["browser"] is None:
-            status["state"] = "running"
-            status["link"] = '''<link rel="run" href="web-1"/>'''
+        if status['browser'] is None:
+            status['state'] = 'running'
+            status['link'] = '''<link rel='run' href='web-1'/>'''
             appurl = render(self.url).substitute(query=self.request.body)
-            status["browser"] = Browser(appurl)
-            status["connectionSvcURL"] = "http://%s/connection/%s" % (
+            status['browser'] = Browser(appurl)
+            status['connectionSvcURL'] = 'http://%s/connection/%s' % (
                 self.ip, self.get_name())
-            status["protocols"] = self.protocols
-            status["app"] = App.get_instance(sec)
+            status['protocols'] = self.protocols
+            status['app'] = App.get_instance(sec)
 
         self.set_app_status(status)
         self.finish()
@@ -124,14 +124,14 @@ class LEAP(tornado.web.RequestHandler):
     def get(self, sec):
         '''Status of an app'''
         self.clear()
-        browser = self.get_app_status()["browser"]
+        browser = self.get_app_status()['browser']
         if not browser:
-            logging.debug("App crashed or closed")
+            logging.debug('App crashed or closed')
             # app crashed or closed
             status = self.get_status_dict()
-            status["state"] = "stopped"
-            status["link"] = ""
-            status["browser"] = None
+            status['state'] = 'stopped'
+            status['link'] = ''
+            status['browser'] = None
             self.set_app_status(status)
 
         self._response()
@@ -140,21 +140,21 @@ class LEAP(tornado.web.RequestHandler):
     def delete(self, sec):
         '''Close app'''
         self.clear()
-        browser = self.get_app_status()["browser"]
+        browser = self.get_app_status()['browser']
         if browser is not None:
             browser.destroy()
         else:
-            logging.warning("App already closed in destroy()")
+            logging.warning('App already closed in destroy()')
         status = self.get_status_dict()
-        status["state"] = "stopped"
-        status["link"] = ""
-        status["browser"] = None
+        status['state'] = 'stopped'
+        status['link'] = ''
+        status['browser'] = None
 
         self.set_app_status(status)
         self._response()
 
     def _getLocation(self, app):
-        return "http://%s/apps/%s/web-1" % (self.ip, app)
+        return 'http://%s/apps/%s/web-1' % (self.ip, app)
 
     def _toXML(self, data):
         return render(self.service).substitute(data)
@@ -162,6 +162,6 @@ class LEAP(tornado.web.RequestHandler):
     @classmethod
     def toInfo(cls):
         data = copy.deepcopy(cls.application_status)
-        data["name"] = cls.__name__
+        data['name'] = cls.__name__
         data = Environment.global_status.get(cls.__name__, data)
         return render(cls.service).substitute(data)
