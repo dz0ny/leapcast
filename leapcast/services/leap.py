50d52d70901107baade2de85aa7599c2658141d1
Jan-Henrik Bruhn <jh.bruhn@me.com>
2013-09-30 23:08:29 +0200

    added support for googles chromecast directory server

--- leapcast/services/leap.py
+++ leapcast/services/leap.py
@@ -4,6 +4,8 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import logging
+import json
+import requests
 from leapcast.apps.default import *
 from leapcast.services.dial import DeviceHandler, ChannelFactory
 from leapcast.services.websocket import ServiceChannel, ReceiverChannel, ApplicationChannel, CastPlatform
@@ -24,12 +26,39 @@ class LEAPserver(object):
             (r"/system/control", CastPlatform),
         ]
 
+        #download apps from google servers
+        logging.info('Loading Config-JSON from Google-Server')
+        app_dict_url = 'https://clients3.google.com/cast/chromecast/device/config'
+        #load json-file
+        resp = requests.get(url = app_dict_url)
+        logging.info('Parsing Config-JSON')
+        #parse json-file
+        data = json.loads(resp.content.replace(")]}'", ""))
+        #list of added apps for apps getting added manually
+        added_apps = []
+        
+        for app in data['applications']:
+            name = app['app_name']
+            name = name.encode('utf-8')
+            if not app.has_key('url'):
+                logging.warn('Didn\'t add %s because it has no URL!' % name)
+                continue
+            logging.info('Added %s app' % name)
+            url = app['url']
+            url = url.replace("${{URL_ENCODED_POST_DATA}}", "{{ query }}").replace("${POST_DATA}", "{{ query }}")
+            #this doesn't support all params yet, but seems to work with youtube, chromecast and play music.
+            clazz = type((name), (LEAPfactory,), {"url": url})
+            routes.append(("(/apps/" + name + "|/apps/" + name + ".*)", clazz))
+            added_apps.append(name)
+            
         #add registread apps
         for app in LEAPfactory.get_subclasses():
             name = app.__name__
+            if name in added_apps:
+                continue
             logging.info('Added %s app' % name)
             routes.append((
-                r"(/apps/" + name + "|/apps/" + name + ".*)", app))
+               r"(/apps/" + name + "|/apps/" + name + ".*)", app))
 
         self.application = tornado.web.Application(routes)
         self.application.listen(8008)
