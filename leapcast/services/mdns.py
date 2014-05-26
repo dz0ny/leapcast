cffbe9e66afe7b55c0ed5e096b05c04965b272ca
dz0ny <dz0ny@ubuntu.si>
2014-05-26 11:12:46 +0200

    Fix: add default ttl

--- leapcast/services/mdns.py
+++ leapcast/services/mdns.py
@@ -9,9 +9,7 @@ import logging
 from SocketServer import ThreadingUDPServer, DatagramRequestHandler
 from time import sleep
 
-from dnslib import PTR, QTYPE, RR, DNSRecord, SRV, A, RD, BimapError, \
-    DNSError, DNSHeader
-from impacket.dns import DNS, DNSType
+from dnslib import PTR, QTYPE, RR, DNSRecord, SRV, A, RD, DNSHeader, DNSLabel
 
 from leapcast.environment import Environment
 from leapcast.utils import ControlMixin
@@ -21,26 +19,8 @@ def mDNSHeader():
     header = DNSHeader(id=0, ra=1, aa=1, qr=1)
     return DNSRecord(header)
 
+
 class MTXT(RD):
-    @classmethod
-    def parse(cls, buffer, length):
-        try:
-            (txtlength,) = buffer.unpack("!B")
-            # First byte is TXT length (not in RFC?)
-            if txtlength < length:
-                data = buffer.get(txtlength)
-            else:
-                raise DNSError("Invalid TXT record: len(%d) > RD len(%d)" %
-                               (txtlength, length))
-            return cls(data)
-        except (BufferError, BimapError) as e:
-            raise DNSError("Error unpacking TXT [offset=%d]: %s" %
-                           (buffer.offset, e))
-
-    @classmethod
-    def fromZone(cls, rd, origin=None):
-        return cls(rd[0].encode())
-
     def __init__(self, txts=[]):
         self.multiple_texts = txts
 
@@ -56,6 +36,31 @@ class MTXT(RD):
         # Pyyhon 2/3 hack
         return ' '.join(self.multiple_texts)
 
+class NSEC(RD):
+
+    def __init__(self, target=None):
+
+        self.target = target
+
+    def set_target(self, target):
+        if isinstance(target, DNSLabel):
+            self._target = target
+        else:
+            self._target = DNSLabel(target)
+
+    def get_target(self):
+        return self._target
+
+    target = property(get_target, set_target)
+
+    def pack(self, buffer):
+        #buffer.pack("!HHH",self.priority,self.weight,self.port)
+        buffer.encode_name(self.target)
+
+    def __repr__(self):
+        return "%s" % self.target
+
+    attrs = 'target'
 
 def GetInterfaceAddress(if_name):
     import fcntl  # late import as this is only supported on Unix platforms.
@@ -127,6 +132,58 @@ class MulticastServer(ControlMixin, ThreadingUDPServer):
         self.handle_membership(socket.IP_DROP_MEMBERSHIP)
 
 
+def mDNSResponse(name, ip, ttl):
+    """
+    Create mDns response packet
+
+    :param name: string Friendly name
+    :param ip: string address of device
+    :param ttl: Time to store response
+    :return: DNSHeader object
+    """
+    res = mDNSHeader()
+    res.add_answer(RR(
+        "_googlecast._tcp.local",
+        QTYPE.PTR,
+        rdata=PTR("%s._googlecast._tcp.local" % name),
+        ttl=ttl
+    ))
+    res.add_ar(RR(
+        "%s._googlecast._tcp.local" % name,
+        QTYPE.TXT,
+        rdata=MTXT([
+            'id=99e46e880f89a46ae627f2e75278cb46',
+            've=02',
+            'md=Chromecast',
+            'ic=/setup/icon.png',
+        ]),
+        ttl=ttl
+    ))
+    res.add_ar(RR(
+        "%s._googlecast._tcp.local" % name,
+        QTYPE.SRV,
+        rdata=SRV(port=8009, target='%s.local' % name),
+        ttl=120
+    ))
+    res.add_ar(RR(
+        "%s.local" % name,
+        QTYPE.A,
+        rdata=A(ip),
+        ttl=120
+    ))
+    # res.add_ar(RR(
+    #     "%s._googlecast._tcp.local" % name,
+    #     QTYPE.NSEC,
+    #     rdata=NSEC(target="%s._googlecast._tcp.local" % name)
+    # ), ttl=ttl)
+    # res.add_ar(RR(
+    #     "%s.local" % name,
+    #     QTYPE.NSEC,
+    #     rdata=NSEC(target="%s.local" % name)
+    # ), ttl=120)
+    return res.pack()
+
+
 class MDNSHandler(DatagramRequestHandler):
     def handle(self):
         data = self.request[0].strip()
@@ -149,52 +206,20 @@ class MDNSHandler(DatagramRequestHandler):
         if not len(datagram) > 0:
             return
         try:
-            query = DNS(datagram).get_questions()
-            qname, qtype, qclass = query.pop()
+            req = DNSRecord.parse(datagram)
+            query = req.get_q()
         except Exception:
             return
 
-        if '_googlecast._tcp.local' in qname and DNSType.PTR == qtype:
+        if '_googlecast._tcp.local' in str(query.get_qname()) and 12 == query \
+                .qtype and query.qclass == 1:
             ip = self.get_remote_ip(address)
             name = Environment.friendlyName
-            res = mDNSHeader()
-            res.add_answer(RR(
-                "_googlecast._tcp.local",
-                QTYPE.PTR,
-                rdata=PTR("%s._googlecast._tcp.local" % name),
-                ttl=3615
-            ))
-            res.add_ar(RR(
-                "%s._googlecast._tcp.local" % name,
-                QTYPE.TXT,
-                rdata=MTXT([
-                    'id=99e46e880f89a46ae627f2e75278cb46',
-                    've=02',
-                    'md=Chromecast',
-                    'ic=/setup/icon.png',
-                ]), ttl=3615))
-            res.add_ar(RR(
-                "%s._googlecast._tcp.local" % name,
-                QTYPE.SRV,
-                rdata=SRV(port=8009, target='%s.local' % name)
-            ))
-            res.add_ar(RR(
-                "%s.local" % name,
-                QTYPE.A,
-                rdata=A(ip)
-            ))
-            sleep(1)
-            self.reply(
-                res.pack(), ('224.0.0.251', 5353)
-            )
+            res = mDNSResponse(name, ip, 3600)
+            sleep(1)  # avoid multicast collision with avahi and others
+            self.reply(res, ('224.0.0.251', 5353))
         else:
-            print(DNS(datagram))
-
-
-def iptohex(self, ip):
-    return b''.join(
-        ['{:x}'.format(int(x)).zfill(2) for x in ip.split('.')]
-    )
+            print(req.get_q().get_qname())
 
 
 class MDNSserver(object):
@@ -208,8 +233,10 @@ class MDNSserver(object):
             (self.MDNS_ADDR, self.MDNS_PORT), MDNSHandler,
             interfaces=interfaces)
         self.server.start()
+        # TODO: Advertise itself on wire
 
     def shutdown(self):
         logging.info('Stopping MDNS server')
+        # TODO: Flush all network caches with ttl 0
         self.server.server_close()
         self.server.stop()
